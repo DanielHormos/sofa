2298bd6ab066eff57d1fc69401477bb4878929fa
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-14 09:20:25 +0100

    added alert click on function when clicking the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 export default function Home() {
+  function onClick() {
+    alert("Clicked");
+  }
+
   return (
     <>
       <h1>Add a sofa</h1>
       <input type="text" />
-      <button>Add sofa</button>
+      <button onClick={onClick}>Add sofa</button>
     </>
   );
 }
